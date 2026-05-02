1b9ea64bc51b225d31e10e095fbc56975dd46b87
Vidyanand Yadav <deepuprince91@gmail.com>
2026-05-02 11:11:01 +0530

    Add solution for counting rotated digits

---
+++ Day-01-Rotated Digits/solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rotatedDigits(int n) {
+
+        int count = 0;
+        for(int i = 0; i <= n; i++){
+            if(isGood(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    private boolean isGood(int num){
+        boolean different = false;
+
+        while(num>0){
+            int digit = num % 10;
+
+            if(digit == 3 || digit == 4 || digit == 7){
+                return false;
+            }
+            if(digit == 2 || digit == 5 || digit == 6 || digit == 9){
+                different = true;
+            }
+            num = num / 10;
+        }
+        return different;
+    }
+
+}
